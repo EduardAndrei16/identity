8bc5ed4bbe8b65b5e64897a834720552604c4763
EduardAndrei16 <83978154+EduardAndrei16@users.noreply.github.com>
2025-05-30 21:53:17 +0300

    Delete identity_project directory

--- identity_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('accounts/', include(("okta_oauth2.urls", "okta_oauth2"), namespace="okta_oauth2")),
-]
